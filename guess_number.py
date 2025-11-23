53344970e2dc98bd5c59b3db89dabaa529d19414
joshuawilhite <jpwilhite2@gmail.com>
2025-11-23 09:30:54 -0600

    Updated project: added a guess counter

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,23 @@
 import random
 
 print("Welcome to Guess the Number!")
-number = random.randint(1,50)
+print("I'm thinking of a number between 1 and 50.")
+number = random.randint(1,50) #this will generate a random number between 1 and 50
 
-while True:
-    guess = int(input("Pick a number between 1 and 50: "))
+guesses = 0
+
+while True: #this will keep the game running until the user guesses the correct number
+    guess_input = input("Take a guess: ") #this will take the user's input as a string
+    if not guess_input.isdigit(): #this checks if the input is a valid number
+        print("Please enter a valid number.")
+        continue
+    guess = int(guess_input)
+    guesses += 1
     if guess < number:
-        print("Too low! Try again! If you want to.")
+        print("Too low! Try again!")
     elif guess > number:
-        print("Too high! Try again! If you want to.")
+        print("Too high! Try again!")
     else:
         print("You got it!")
+        print(f"It took you {guesses} tries!")
         break
